4b82544991bcc0d2590dffe853aa4557ffd29a2b
Sven Nilsen <bvssvni@gmail.com>
2016-03-08 22:16:08 +0100

    Update dependencies
    - Bumped to 0.2.0
    - Improved docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,11 @@
-extern crate shaders_graphics2d;
+#![deny(missing_docs)]
 
-pub use shaders_graphics2d as graphics2d;
+//! Shaders for game development in Rust
+//!
+//! This library is intended only for reexporting various shaders
+//! that are useful for game development.
+//!
+//! Libraries should depend directly on the specific shader library.
+
+pub extern crate shaders_graphics2d as graphics2d;
+
